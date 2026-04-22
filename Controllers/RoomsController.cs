aeeb69f215d937cd7d28bf84db0102ec2fe288a0
MicPJ <porzucmichal@gmail.com>
2026-04-22 03:18:37 +0200

    Added query filter and rooms by building controller

--- Controllers/RoomsController.cs
+++ Controllers/RoomsController.cs
@@ -9,9 +9,23 @@ namespace TrainingCenterApp.Controllers
     public class RoomsController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IEnumerable<Room>> GetAllRooms()
+        public ActionResult<IEnumerable<Room>> GetAllRooms(
+            [FromQuery] int? minCapacity,
+            [FromQuery] bool? hasProjector,
+            [FromQuery] bool? activeOnly)
         {
-            return Ok(DataContext.Rooms);
+            var rooms = DataContext.Rooms.AsEnumerable();
+
+            if (minCapacity.HasValue)
+                rooms = rooms.Where(r => r.Capacity >= minCapacity.Value);
+
+            if (hasProjector.HasValue)
+                rooms = rooms.Where(r => r.HasProjector == hasProjector.Value);
+
+            if (activeOnly.HasValue && activeOnly.Value)
+                rooms = rooms.Where(r => r.IsActive);
+
+            return Ok(rooms.ToList());
         }
 
         
@@ -24,6 +38,14 @@ namespace TrainingCenterApp.Controllers
                 return NotFound();
             }
             return Ok(room);
+        }
+
+            
+        [HttpGet("building/{buildingCode}")]
+        public ActionResult<IEnumerable<Room>> GetRoomsByBuilding(string buildingCode)
+        {
+            var rooms = DataContext.Rooms.Where(r => r.BuildingCode == buildingCode).ToList();
+            return Ok(rooms);
         }
     }
 }
--- obj/Debug/net9.0/TrainingCenterApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TrainingCenterApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingCenterApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c8773c581575355ef1d112b0e84108915c86f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedf43e75dfac6661213f2c78ef42c97b6b59d40")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingCenterApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingCenterApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
